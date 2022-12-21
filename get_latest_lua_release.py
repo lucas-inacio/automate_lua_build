ff04eb3f4f35ebff2a1be01b883004646143d3c3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-21 15:53:50 +0000

    Adding tarfile member sanitization to extractall()

--- get_latest_lua_release.py
+++ get_latest_lua_release.py
@@ -15,7 +15,26 @@ if __name__ == "__main__":
         download_url = j['tarball_url']
         filename, headers = request.urlretrieve(download_url)
         with tarfile.open(filename) as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
             os.rename(tar.getnames()[0], './lua')
     except:
         print('Error')
